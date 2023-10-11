18a3a0133a3a171c20c379359d8eaaa3403b89ec
robertop <robertop@accudiresrl.com>
2023-10-11 10:02:17 +0200

    add fibonacci function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci sequence");
+    println!("Enter an index for the sequence: ");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index = index.trim().parse::<u8>().unwrap();
+
+    println!("The {index}-th element of the sequence is: {}", fibonacci(index))
+}
+
+fn fibonacci(n: u8) -> u16 {
+    if n == 0 { 0 } else if n == 1 { 1 } else { fibonacci(n - 2) + fibonacci(n - 1) }
 }
